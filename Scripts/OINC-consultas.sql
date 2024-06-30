b7ff1f1b35ea68d593f86a7701b4192ad920035d
Miguel Schneiders Flach <165222126+MiguelSFlach@users.noreply.github.com>
2024-06-30 16:56:38 -0300

    Update OINC-consultas.sql

--- Scripts/OINC-consultas.sql
+++ Scripts/OINC-consultas.sql
@@ -23,34 +23,58 @@ order by a.aniid;
 -- 1-Relacionar todos os animais com idades entre 6 a 10 meses e do sexo masculino. Ordenar o relatório pelo tipo do animal em ordem descendente:
 
 
-SELECT aniid, tipani, datnas, pesani, stsani, caumor, Grupogruid, Granjacnpj
-FROM Animal
-WHERE tipani = 2
-AND AGE(datnas) BETWEEN INTERVAL '6 months' AND INTERVAL '10 months'
-ORDER BY tipani DESC;
+-- 1 Animais com idades entre 6 a 10 meses e do sexo masculino
+SELECT DISTINCT a.aniid "ID do Animal", 
+                a.tipani "Tipo do Animal", 
+                a.datnas "Data de Nascimento", 
+                a.pesani "Peso do Animal", 
+                a.stsani "Status do Animal", 
+                a.caumor "Causa da Morte", 
+                a.Grupogruid "ID do Grupo", 
+                a.Granjacnpj "CNPJ da Granja"
+FROM Animal a
+WHERE a.tipani = 2 -- 2 indica que o animal é masculino
+AND AGE(a.datnas) BETWEEN INTERVAL '6 months' AND INTERVAL '10 months'
+ORDER BY a.tipani DESC;
+
 
 -- 2-Relacionar as granjas das cidades de São Miguel do Oeste e Descanso. Ordene o relatório pelo nome da granja em forma ascendente:
 
-SELECT Granja.cnpj, nomgra, telgran, qtdanigra
-FROM Granja
-JOIN Endereco ON Granja.cnpj = Endereco.Granjacnpj
-WHERE Endereco.cidend IN ('São Miguel do Oeste', 'Descanso')
-ORDER BY nomgra ASC;
+-- 2 Granjas das cidades de São Miguel do Oeste e Descanso
+SELECT DISTINCT g.cnpj "CNPJ da Granja", 
+                g.nomgra "Nome da Granja", 
+                g.telgran "Telefone da Granja", 
+                g.qtdanigra "Quantidade de Animais", 
+                e.cidend "Cidade"
+FROM Granja g
+INNER JOIN Endereco e ON g.cnpj = e.Granjacnpj
+WHERE e.cidend IN ('São Miguel do Oeste', 'Descanso')
+ORDER BY g.nomgra ASC;
+
 
 -- 3-Relacionar todas as granjas das cidades de Maravilha, Descanso, Itapiranga e Guaraciaba que criaram mais de 100 animais. Ordene o relatório da cidade com mais animais para a cidade com menos animais:
 
-SELECT Granja.cnpj, nomgra, telgran, qtdanigra, Endereco.cidend
-FROM Granja
-JOIN Endereco ON Granja.cnpj = Endereco.Granjacnpj
-WHERE Endereco.cidend IN ('Maravilha', 'Descanso', 'Itapiranga', 'Guaraciaba')
-AND qtdanigra > 100
-ORDER BY qtdanigra DESC;
+-- 3 Granjas das cidades de Maravilha, Descanso, Itapiranga e Guaraciaba com mais de 100 animais
+SELECT DISTINCT g.cnpj "CNPJ da Granja", 
+                g.nomgra "Nome da Granja", 
+                g.telgran "Telefone da Granja", 
+                g.qtdanigra "Quantidade de Animais", 
+                e.cidend "Cidade"
+FROM Granja g
+INNER JOIN Endereco e ON g.cnpj = e.Granjacnpj
+WHERE e.cidend IN ('Maravilha', 'Descanso', 'Itapiranga', 'Guaraciaba')
+AND g.qtdanigra > 100
+ORDER BY g.qtdanigra DESC;
+
 
 -- 4-Relacionar a granja, os tipos de animais e o total de animais por granja/tipo. Relacionar a granja com mais animais para a instituição com menos animais:
 
-SELECT Granja.nomgra, Animal.tipani, COUNT(Animal.aniid) AS total_animais
-FROM Granja
-JOIN Animal ON Granja.cnpj = Animal.Granjacnpj
-GROUP BY Granja.nomgra, Animal.tipani
-ORDER BY total_animais DESC;
+-- 4 Granja, tipos de animais e total de animais por granja/tipo
+SELECT DISTINCT g.nomgra "Nome da Granja", 
+                a.tipani "Tipo do Animal", 
+                COUNT(a.aniid) AS "Total de Animais"
+FROM Granja g
+INNER JOIN Animal a ON g.cnpj = a.Granjacnpj
+GROUP BY g.nomgra, a.tipani
+ORDER BY "Total de Animais" DESC;
 
