b6e9be3f963af6a385c904140f123480e7e0f429
Miguel Schneiders Flach <165222126+MiguelSFlach@users.noreply.github.com>
2024-11-21 20:24:55 -0300

    Add files via upload

---
+++ Oinc-System/src/main/java/com/granja/oinc_system/Application.java
@@ -0,0 +1,16 @@
+package com.granja.oinc_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
