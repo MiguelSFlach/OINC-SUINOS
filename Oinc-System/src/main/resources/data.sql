facfeec65674434d91d90a2814a7297094eeff35
Miguel Schneiders Flach <165222126+MiguelSFlach@users.noreply.github.com>
2024-11-21 20:26:20 -0300

    Add files via upload

---
+++ Oinc-System/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Usuario (nomusu, emausu, senusu, tipusu, cpfusu, sexusu, idausu, Granjacnpj) 
+VALUES ('Admin', 'admin@example.com', 'admin123', 1, 12345678901, 1, 30, 12345678901234);
+
+INSERT INTO Usuario (nomusu, emausu, senusu, tipusu, cpfusu, sexusu, idausu, Granjacnpj) 
+VALUES ('User', 'user@example.com', 'user123', 2, 98765432101, 2, 25, 12345678901234);
